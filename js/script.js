d2a1da78723f825bc579cebf407e3a629c00dd19
ScottToste <Scott.Toste@gmail.com>
2023-03-13 11:10:50 -0400

    Added file for js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-//TODO
+document.getElementById("linkedinButton").onclick = function () 
+{
+    location.href = "https://www.linkedin.com/in/scott-toste-182246262/";
+};
+
+document.getElementById("githubButton").onclick = function () 
+{
+    location.href = "https://github.com/ScottToste";
+};
